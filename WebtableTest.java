4b5a1526f1446c2c85f939eaac3e46aff9b5e3c1
uraghuvardhan08 <117755381+uraghuvardhan08@users.noreply.github.com>
2022-11-16 10:13:02 +0530

    Add files via upload

---
+++ WebtableTest.java
@@ -0,0 +1,17 @@
+ //table[@id='customers']
+
+ //table[contains(@id,'cust')]//th
+
+
+//columns
+//table[@id='customers']//th
+
+//all data
+//table[@id='customers']//td
+
+
+//only second column
+//table[@id='customers']//td[2]
+
+
+
